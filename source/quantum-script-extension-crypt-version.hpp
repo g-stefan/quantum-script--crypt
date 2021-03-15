813c242539a6ab0b93c52a8f4e8e5d3a0ed4bc29
Grigore Stefan <g_stefan@yahoo.com>
2021-03-15 12:36:17 +0200

    project-structure

---
+++ source/quantum-script-extension-crypt-version.hpp
@@ -0,0 +1,41 @@
+//
+// Quantum Script Extension Crypt
+//
+// Copyright (c) 2020-2021 Grigore Stefan <g_stefan@yahoo.com>
+// Created by Grigore Stefan <g_stefan@yahoo.com>
+//
+// MIT License (MIT) <http://opensource.org/licenses/MIT>
+//
+
+#ifndef QUANTUM_SCRIPT_EXTENSION_CRYPT_VERSION_HPP
+#define QUANTUM_SCRIPT_EXTENSION_CRYPT_VERSION_HPP
+
+#define QUANTUM_SCRIPT_EXTENSION_CRYPT_VERSION_ABCD                 1,4,0,5
+#define QUANTUM_SCRIPT_EXTENSION_CRYPT_VERSION_STR                 "1.4.0"
+#define QUANTUM_SCRIPT_EXTENSION_CRYPT_VERSION_STR_BUILD           "5"
+#define QUANTUM_SCRIPT_EXTENSION_CRYPT_VERSION_STR_DATETIME        "2021-03-15 12:30:22"
+
+#ifndef XYO_RC
+
+#ifndef QUANTUM_SCRIPT_EXTENSION_CRYPT__EXPORT_HPP
+#include "quantum-script-extension-crypt--export.hpp"
+#endif
+
+namespace Quantum {
+	namespace Script {
+		namespace Extension {
+			namespace Crypt {
+				namespace Version {
+					QUANTUM_SCRIPT_EXTENSION_CRYPT_EXPORT const char *version();
+					QUANTUM_SCRIPT_EXTENSION_CRYPT_EXPORT const char *build();
+					QUANTUM_SCRIPT_EXTENSION_CRYPT_EXPORT const char *versionWithBuild();
+					QUANTUM_SCRIPT_EXTENSION_CRYPT_EXPORT const char *datetime();
+				};
+			};
+		};
+	};
+};
+
+#endif
+#endif
+
