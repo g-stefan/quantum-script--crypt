7af6421d257d45f0f71082a0ce0aa4bffa971b78
Grigore Stefan <g_stefan@yahoo.com>
2022-01-01 21:25:29 +0200

    Update

--- source/quantum-script-extension-crypt-version.cpp
+++ source/quantum-script-extension-crypt-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace Crypt {
 				namespace Version {
 
-					static const char *version_ = "2.6.0";
-					static const char *build_ = "20";
-					static const char *versionWithBuild_ = "2.6.0.20";
-					static const char *datetime_ = "2021-11-22 17:17:33";
+					static const char *version_ = "2.10.0";
+					static const char *build_ = "21";
+					static const char *versionWithBuild_ = "2.10.0.21";
+					static const char *datetime_ = "2022-01-01 21:19:39";
 
 					const char *version() {
 						return version_;
